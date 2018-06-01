d548eefd1e0f79b4ae3bd0eb6aa2b3e77d0e415d
Luis Gustavo Araujo <luisaraujo.ifba@gmail.com>
2018-05-31 22:44:06 -0300

    add page classe
    Add page of Programacao Orientada

---
+++ aulas/unifacs/disciplinas/lin_comp/2018/aulas/teste.js
@@ -0,0 +1,14 @@
+nome = "PROGRAMA DE SOMA";
+num1 = 3;
+num2 = prompt("informe um número");
+
+soma = num1 + num2;
+subtracao = num1 - num2;
+produto = num1 * num2;
+divisao = num1 / num2;
+
+console.log("SOMA e SUBTRAÇÃO")
+console.log(soma, subtracao)
+console.log("PRODUTO", produto)
+console.log("DIVISÃO")
+console.log(divisao)
